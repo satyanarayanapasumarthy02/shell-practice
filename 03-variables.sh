e7db8ffeba96beb5eef532d2266d92e8bfa3c702
Satyanarayana P <satyanarayana.pasumarthy02@gmail.com>
2025-09-22 20:57:54 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# No space before or after equal
+PERSON1=Jhon
+PERSON2=Raheem
+echo "$PERSION1:: Hi $PERSION2, How are you?"
+echo "$PERSION2:: Hello $PERSION1, i am fine. How are you?"
+echo "$PERSION1:: What are you learning recently?"
+echo "$PERSION2:: I am learning Shell scripting $PERSION1"
