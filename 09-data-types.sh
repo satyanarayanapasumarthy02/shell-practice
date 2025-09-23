d1cf3244740b1c655ca1925c61ef61a33001233c
Satyanarayana P <satyanarayana.pasumarthy02@gmail.com>
2025-09-23 19:45:47 +0530

    data types variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Everything in shell is considerd as string
+# everything in shell is considerd as string
 NUMBER1=100
 NUMBER2=200
 
-SUM=$(($NUMBER1+NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
-echo "SUM is: $(SUM)"
+echo "SUM is: ${SUM}"
