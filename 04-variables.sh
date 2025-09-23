4a5034d6d92d393994318de394fe6593ad069574
Satyanarayana P <satyanarayana.pasumarthy02@gmail.com>
2025-09-23 17:29:44 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to ALsaka $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom webiner"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, If you make Ukrine joins Russia"
