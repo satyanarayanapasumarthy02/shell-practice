5436698b95b8dd40c75f4e88b966f0d0000ca360
Satyanarayana P <satyanarayana.pasumarthy02@gmail.com>
2025-09-24 16:44:40 +0530

    data types if else

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,8 +4,10 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is less than 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "Given number $NUMBER is equal to 10"    
 else
-    echo "Given number $NUMBER is grater than or equal to 10"
+    echo "Given number $NUMBER is grater than or to 10"
 fi 
 
 
