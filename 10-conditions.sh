4d565515a9d16c75032488fbfbe064dd30a49cc3
Satyanarayana P <satyanarayana.pasumarthy02@gmail.com>
2025-09-24 16:25:42 +0530

    data types if else

---
+++ 10-conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $NUMBER -lt 10 ]; then
+    echo "Given number $NUMBER is less than 10"
+else
+    echo "Given number $NUMBER is grater than or equal to 10"
+fi 
+
+
+
+# -gt
+# -eq
+# -ne
+
+
+
+
+    
